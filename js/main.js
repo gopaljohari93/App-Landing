6e16c909bf207212fe55112066355f3fc19f0048
gopaljohari <gopaljohari007@gmail.com>
2023-10-07 17:21:21 +0530

    first commit app-landing

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// Header Scroll Navbar JavaScript 
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("header-scrolled");
+    } else {
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+// nav hide
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
+        navCollapse.classList.remove("show");
+    })
+})
+
+// Owl Carousel JavaScript  
+$(document).ready(function () {
+    $('.slider-img-section').owlCarousel({
+        items: 4,
+        loop: true,
+        margin: 10,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        autoplayHoverPause: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2,
+            },
+            600: {
+                items: 3,
+            },
+            1000: {
+                items: 6,
+            }
+        }
+    })
+})
